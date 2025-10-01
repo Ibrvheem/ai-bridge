6fd77abcd67b8a9546610de14449dcbc6a889749
ibrahim <i.aliyu019@gmail.com>
2025-10-01 23:57:22 +0100

    feat: reset form after successfully adding a language

--- app/(dashboard)/settings/languages/_hooks/use-language-form.ts
+++ app/(dashboard)/settings/languages/_hooks/use-language-form.ts
@@ -22,6 +22,7 @@ export function useLanguageForm() {
             const response = createLanguage(data)
             toast.success("Language added successfully")
             setOpen(false)
+            form.reset({})
             revalidateLanguages()
             return response
         } catch (error) {
