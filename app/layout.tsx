5a0b7bdc88fe2646012e03d2cd26b00e4f39ac6b
ibrahim <i.aliyu019@gmail.com>
2025-10-16 14:26:05 +0100

    feat: Enable dynamic rendering in layout by adding dynamic export

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+// app/(dashboard)/layout.tsx
+export const dynamic = "force-dynamic";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
